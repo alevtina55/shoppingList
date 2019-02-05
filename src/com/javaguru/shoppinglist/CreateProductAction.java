c4fe0df6927cf0a91400e4f4160ea97a6c883c30
alevtina55 <alia131196@gmail.com>
2019-02-05 22:55:42 +0200

    New fields and validation improvements

--- src/com/javaguru/shoppinglist/CreateProductAction.java
+++ src/com/javaguru/shoppinglist/CreateProductAction.java
@@ -9,6 +9,8 @@ public class CreateProductAction implements Action {
 
     private final ProductService productService;
 
+    private final Validator validator = new Validator();
+
     public CreateProductAction(ProductService productService) {
         this.productService = productService;
     }
@@ -16,14 +18,64 @@ public class CreateProductAction implements Action {
     @Override
     public void execute() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter product name:");
-        String name = scanner.nextLine();
-        System.out.println("Enter product price: ");
-        String price = scanner.nextLine();
+        boolean inputIsInvalid = true;
+        String name;
+        BigDecimal price = null;
+        BigDecimal discount = null;
+
+        do {
+            System.out.println("Enter product name:");
+            name = scanner.nextLine();
+            try {
+                validator.validateName(name);
+                inputIsInvalid = false;
+            } catch (ValidationException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Error. Try again!");
+            }
+        } while (inputIsInvalid);
+
+        inputIsInvalid = true;
+        do {
+            System.out.println("Enter product price: ");
+            try {
+                price = new BigDecimal(scanner.nextLine());
+                validator.validatePrice(price);
+                inputIsInvalid = false;
+            } catch (ValidationException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Error. Try again!");
+            }
+        } while (inputIsInvalid);
+
+        System.out.println("Enter product category: ");
+        String category = scanner.nextLine();
+
+        inputIsInvalid = true;
+        do {
+            System.out.println("Enter product discount: ");
+            try {
+                discount = new BigDecimal(scanner.nextLine());
+                validator.validateDiscount(discount);
+                inputIsInvalid = false;
+            } catch (ValidationException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Error. Try again!");
+            }
+        } while (inputIsInvalid);
+
+        System.out.println("Enter product description: ");
+        String description = scanner.nextLine();
 
         Product product = new Product();
         product.setName(name);
-        product.setPrice(new BigDecimal(price));
+        product.setPrice(price);
+        product.setCategory(category);
+        product.setDiscount(discount);
+        product.setDescription(description);
 
         try {
             Long response = productService.create(product);
---
+++ src/com/javaguru/shoppinglist/Validator.java
@@ -0,0 +1,28 @@
+package com.javaguru.shoppinglist;
+
+import java.math.BigDecimal;
+
+public class Validator {
+    public void validateName(String name) throws ValidationException {
+        if (name.trim().length() < 3) {
+            throw new ValidationException("Name is too short");
+        }
+
+        if (name.trim().length() > 32) {
+            throw new ValidationException("Name is to long");
+        }
+    }
+
+    public void validatePrice(BigDecimal price) throws ValidationException {
+        if (price.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new ValidationException("Price should be greater than 0");
+        }
+    }
+
+    public void validateDiscount(BigDecimal discount) throws ValidationException {
+        if (discount.compareTo(BigDecimal.ZERO) < 0 || discount.compareTo(new BigDecimal(100)) > 0) {
+            throw new ValidationException("Price should not be less than 0 and greater than 100");
+        }
+    }
+
+}
